f2fbeb3847cba0bf399bb9d5903ff6ff3f27cf9a
eigenlambda123 <rmvilla987@gmail.com>
2025-08-08 22:27:30 +0800

    access

---
+++ prelims/activity_2/acess.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // User input
+    int indexUserValue = 0;
+
+    // Initialize Array
+    string CompScieSubjects[4] = {"Linear Algebra", "Probability", "DataStructures and Algorithms", "OOP"};
+
+    // Access value in a specific Index
+    cout << CompScieSubjects[indexUserValue];
+
+}
