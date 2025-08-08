0ff5b3ba8f491ba0a400335196d2f7b4b1457f65
eigenlambda123 <rmvilla987@gmail.com>
2025-08-08 22:39:56 +0800

    Replace

--- prelims/activity_2/replace.cpp
+++ prelims/activity_2/replace.cpp
@@ -4,12 +4,31 @@ using namespace std;
 int main()
 {
     // User input
-    int indexUserValue = 0;
+    int userInputIndex = 0;
     string inputUserValue = "Machine Learning";
 
     // Initialize Array
-    string CompScieSubjects[4] = {"Linear Algebra", "Probability", "DataStructures and Algorithms", "OOP"};
+    string compScieSubjects[4] = {"Linear Algebra", "Probability", "DataStructures and Algorithms", "OOP"};
+
+
+    // Size of the Array
+    int lenthOfArray = 4;
+
+
+    // Before Array
+    cout << "Before Array: ";
+    for (int i=0; i<lenthOfArray; i++) {
+        cout << compScieSubjects[i] << " ";
+    }
 
     // Replace
-    CompScieSubjects[indexUserValue] = "inputUserValue";
+    compScieSubjects[userInputIndex] = inputUserValue;
+
+
+    // After Array
+    cout << "\nAfter Array: ";
+    for (int i=0; i<lenthOfArray; i++) {
+        cout << compScieSubjects[i] << " ";
+    }
+
 }
