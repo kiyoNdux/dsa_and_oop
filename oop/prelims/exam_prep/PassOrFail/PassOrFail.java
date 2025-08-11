cf3818a52b67fd58fd8108ee5513e002b436161a
eigenlambda123 <rmvilla987@gmail.com>
2025-08-11 22:54:42 +0800

    PassOrFail

--- oop/prelims/exam_prep/PassOrFail/PassOrFail.java
+++ oop/prelims/exam_prep/PassOrFail/PassOrFail.java
@@ -3,12 +3,15 @@ import javax.swing.JOptionPane;
 class PassOrFail {
 	public static void main(String[] args) {
 		float grade;
+		String name;
+
+		name = JOptionPane.showInputDialog("What is your Name: ");
 		grade = Float.parseFloat(JOptionPane.showInputDialog("What is your Grade: "));
 
 		if (grade >= 75) {
-			JOptionPane.showMessageDialog(null, "You passed.", "Message", JOptionPane.INFORMATION_MESSAGE);	
+			JOptionPane.showMessageDialog(null, "Name: " + name +" Status: passed.", "Message", JOptionPane.INFORMATION_MESSAGE);	
 		} else {
-			JOptionPane.showMessageDialog(null, "You failed.", "Message", JOptionPane.INFORMATION_MESSAGE);	
+			JOptionPane.showMessageDialog(null, "Name: " + name + "Status: failed.", "Message", JOptionPane.INFORMATION_MESSAGE);	
 		}
 	}
 }
