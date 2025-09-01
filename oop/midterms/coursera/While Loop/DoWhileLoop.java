0ad65bfef074ff2a1af1d69137ef76b1f1f9e749
eigenlambda123 <rmvilla987@gmail.com>
2025-09-01 08:57:18 +0800

    While Loop

---
+++ oop/midterms/coursera/While Loop/DoWhileLoop.java
@@ -0,0 +1,11 @@
+public class DoWhileLoop{
+    public static void main(String[] args) {
+        int count = 20; 
+        int n = 20;     
+
+        do {
+            System.out.print("*");
+            count++;
+        } while (count < n);
+    }
+}
