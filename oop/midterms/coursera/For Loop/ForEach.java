10b8d054d84ee77890ce55608fde3f12d7cfdd09
eigenlambda123 <rmvilla987@gmail.com>
2025-09-01 09:02:46 +0800

    ForLoop

---
+++ oop/midterms/coursera/For Loop/ForEach.java
@@ -0,0 +1,10 @@
+public class ForEach{
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Cherry", "Date"};
+
+        // For-each loop to print each fruit
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
