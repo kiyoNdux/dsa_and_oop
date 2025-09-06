f7a368b3494cf4f01d3590afd0ed6dd751d64958
eigenlambda123 <rmvilla987@gmail.com>
2025-09-06 16:55:59 +0800

    cpp-intro

--- dsa/prelims/activity_2/merge/merge.cpp
+++ dsa/prelims/activity_2/merge/merge.cpp
@@ -1,51 +1,50 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    // Array Sizes
-    int listaSize = 3;
-    int listbSize = 3;
+int main() {
+    int lista[] = {1, 2, 3, 4, 5};
+    int listb[] = {3, 4, 5, 6, 7};
+    int listaSize = 5, listbSize = 5;
 
-    // Array Initialization
-    int lista[listaSize] = {1, 2, 3};
-    int listb[listbSize] = {4, 5, 6};
-
-    // Resultant Array (Merged)
     int listc[listaSize + listbSize];
+    int a = 0, b = 0, c = 0;
 
-    int a = 0; // Index for lista
-    int b = 0; // Index for listb
-    int c = 0; // Index for listc
-
-    // Merge
     while (a < listaSize && b < listbSize) {
         if (lista[a] < listb[b]) {
-            listc[c] = lista[a];
+            if (c == 0 || listc[c-1] != lista[a]) // avoid duplicates
+                listc[c++] = lista[a];
             a++;
-        } else {
-            listc[c] = listb[b];
+        }
+        else if (listb[b] < lista[a]) {
+            if (c == 0 || listc[c-1] != listb[b])
+                listc[c++] = listb[b];
             b++;
         }
-        c++;
+        else { // equal values, add only once
+            if (c == 0 || listc[c-1] != lista[a])
+                listc[c++] = lista[a];
+            a++;
+            b++;
+        }
     }
 
-    // Copy Remaining Elements from lista
     while (a < listaSize) {
-        listc[c] = lista[a];
+        if (c == 0 || listc[c-1] != lista[a])
+            listc[c++] = lista[a];
         a++;
-        c++;
     }
 
-    // Copy Remaining Elements from listb
     while (b < listbSize) {
-        listc[c] = listb[b];
+        if (c == 0 || listc[c-1] != listb[b])
+            listc[c++] = listb[b];
         b++;
-        c++;
     }
 
-    cout << "\nMerged Array: ";
-    for (int k = 0; k < (listaSize + listbSize); k++) {
+    cout << "Merged array: ";
+    for (int k = 0; k < c; k++) {
         cout << listc[k] << " ";
     }
+    cout << endl;
+
+    return 0;
 }
