9e29409e6c91f1fb300c9f013f9a99d0593a6ce1
eigenlambda123 <rmvilla987@gmail.com>
2025-09-06 17:32:47 +0800

    custom_namespace

---
+++ dsa/midterm/coursera/week_1/03_standard-library/custom_namespace/Cube.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace uiuc {
+    class Cube{
+        public:
+            double getVolume();
+            double getSurfaceArea();
+            void setLength(double length);
+
+        private:
+            double length_;
+
+    };
+}
+
---
+++ dsa/midterm/coursera/week_1/03_standard-library/custom_namespace/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Cube.h"
+
+using namespace std;
+
+int main()
+{
+    uiuc::Cube c;
+
+    c.setLength(2.4);
+    std::cout << "Volume: " << c.getVolume() <<std::endl;
+
+    double surfaceArea = c.getSurfaceArea();
+    std::cout << "Surface Area: " << surfaceArea <<std::endl;
+    return 0;
+}
