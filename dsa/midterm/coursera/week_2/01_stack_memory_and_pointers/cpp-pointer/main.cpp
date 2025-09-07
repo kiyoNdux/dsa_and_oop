974815eaf29fcb53fc8e79941ee1f6269073a33e
eigenlambda123 <rmvilla987@gmail.com>
2025-09-07 11:12:55 +0800

    cpp-pointer

---
+++ dsa/midterm/coursera/week_2/01_stack_memory_and_pointers/cpp-pointer/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 7;
+    cout << "num: " << num << endl;
+    cout << "&num: " << &num << endl;
+
+    int *p = &num;
+    cout << "p: " << p << endl;
+    cout << "&p: " << &p << endl;
+    cout << "*p: " << *p << endl;
+
+    *p = 42;
+    cout << "*p changed to 42" << endl;
+    cout << "num: " << num << endl;
+
+    return 0;
+
+}
