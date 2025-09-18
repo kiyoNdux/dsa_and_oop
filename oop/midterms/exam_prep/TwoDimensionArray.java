3330bb4e65f0d4738dd3723e4fb27e2cae3a9de1
eigenlambda123 <rmvilla987@gmail.com>
2025-09-18 08:12:48 +0800

    TwoDimensionArray

--- oop/midterms/exam_prep/TwoDimensionArray.java
+++ oop/midterms/exam_prep/TwoDimensionArray.java
@@ -2,17 +2,16 @@ import javax.swing.JOptionPane;
 
 class TwoDimensionArray{
 	public static void main(String[] args) {
-		String inp, str = "";
+		String inp, str="";
 		int arr[][] = new int[2][3];
-		
+
 		for (int i=0; i<2; i++) {
 			for (int j=0; j<3; j++) {
-				inp=JOptionPane.showInputDialog(null, "Input value in Index["+i+"]["+j+"]:");
-				arr[i][j]=Integer.parseInt(inp);
-				str = str+"Index["+i+"]["+j+"]:" + arr[i][j]+"\n";
+				inp = JOptionPane.showInputDialog(null, "Input value in Index:\n\n" + "Index["+i+"]["+j+"]: ");
+				arr[i][j] = Integer.parseInt(inp);
+				str = str + "Index["+i+"]["+j+"]:" + arr[i][j] + "\n";
 			}
 		}
-
-		JOptionPane.showMessageDialog(null,"Input values are:\n\n" + str);
+		JOptionPane.showMessageDialog(null, "Input values are:\n\n"+str);
 	}
 }
