01bf75750492f8e40509ff83cf9ea81616225444
eigenlambda123 <rmvilla987@gmail.com>
2025-09-18 08:30:35 +0800

    Multiplication Table

--- oop/midterms/exam_prep/MultiplicationTable.java
+++ oop/midterms/exam_prep/MultiplicationTable.java
@@ -4,7 +4,7 @@ class MultiplicationTable{
 	public static void main(String[] args) {
 		int multp_arr[] = new int[100];
 		int counter = -1;
-		String str="";
+		StringBuilder str = new StringBuilder();
 		for(int i=1; i<=10; i++) {
 			for(int j=1; j<=10; j++) {
 				counter++;
@@ -13,11 +13,11 @@ class MultiplicationTable{
 			}
 		}
 		for (int k = 0; k < multp_arr.length; k++){
-			str = str + multp_arr[k] + " ";
+			str.append(String.format("%4d", multp_arr[k]));
 			if ((k+1)%10 == 0) {
-				str = str + "\n";
+				str.append("\n");
 			}
 		}
-		JOptionPane.showMessageDialog(null, str);
+		JOptionPane.showMessageDialog(null, str.toString(), "MULTIPLICATION TABLE", JOptionPane.INFORMATION_MESSAGE);
 	}
 }
